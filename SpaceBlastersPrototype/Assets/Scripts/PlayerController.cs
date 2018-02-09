cf15d8b69e3e9a02c3dc0f1d0fdbb134e75dbafb
Patrick Hirsh <hirshpatrick@gmail.com>
2018-02-09 13:45:15 -0700

    Initial Commit

---
+++ SpaceBlastersPrototype/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody2D rb;
+    public float moveSpeed;
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
