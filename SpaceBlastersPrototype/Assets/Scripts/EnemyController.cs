e10c19f3e515d38baf4ddde8544db0be8aa4c295
root <root@Beatrice.localdomain>
2018-02-10 16:31:58 -0700

    Added enemy_01 prefab and corresponding script

--- SpaceBlastersPrototype/Assets/Scripts/EnemyController.cs
+++ SpaceBlastersPrototype/Assets/Scripts/EnemyController.cs
@@ -6,26 +6,29 @@ public class EnemyController : MonoBehaviour {
 
     public Transform target;//set target from inspector instead of looking in Update
     public float speed = 3f;
-
+    public float hitbox = .7f;
+    private Rigidbody2D rb;
 
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
 
         //rotate to look at the player
-        transform.LookAt(target.position);
-        transform.Rotate(new Vector3(0, -90, 0), Space.Self);//correcting the original rotation
+        //transform.LookAt(target.position);
 
-
+        //rb.AddForce(transform.forward * speed);
+        var direction = Vector3.zero;
         //move towards the player
-        if (Vector3.Distance(transform.position, target.position) > 1f)
+        if (Vector3.Distance(transform.position, target.position) > hitbox)
         {//move if distance from target is greater than 1
-            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
-        }
+            direction = target.position - transform.position;
+            rb.AddRelativeForce(direction.normalized * speed, ForceMode2D.Force);
 
+            Mathf.Clamp(rb.velocity.magnitude, .3f, 3f);
+        }
     }
 }
