a6c5b67e17a66bb51a61f00e5b41bef79361926c
Tyler <mr.hoskino@gmail.com>
2018-02-16 13:17:56 -0700

    Added shooting

--- SpaceBlastersPrototype/Assets/Scripts/BulletController.cs
+++ SpaceBlastersPrototype/Assets/Scripts/BulletController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class BulletController : MonoBehaviour {
 
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,13 +12,14 @@ public class BulletController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        
 		
 	}
 
     void OnCollisionEnter2D(Collision2D coll)
     {
-        if (coll.gameObject.tag == "Enemy")
-            coll.gameObject.SendMessage("Hit");
-
+        if (coll.gameObject.tag == "bullet" || coll.gameObject.tag == "Player")
+            Physics2D.IgnoreCollision(coll.collider, this.GetComponent<Collider2D>());
+        
     }
 }
--- SpaceBlastersPrototype/Assets/Scripts/PLayerSynthControls.cs
+++ SpaceBlastersPrototype/Assets/Scripts/PLayerSynthControls.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class PLayerSynthControls : MonoBehaviour
 {
-
+    public GameObject bulletMuzzle;
     private Vector2 velocity;
     public float velocityRate = 20f;
     public float accelerationRate;
     Rigidbody2D rb = new Rigidbody2D();
+    float bulletDistance = 50f;
+    public GameObject bulletPrefab;
 
     // Use this for initialization
     void Start()
@@ -97,6 +99,58 @@ public class PLayerSynthControls : MonoBehaviour
             }
         }
         GetComponent<Rigidbody2D>().AddForce(velocity);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if(Input.mousePosition.x >= 0 && Input.mousePosition.y >= 0)
+            {
+                //Get mouse position
+                Vector3 position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, bulletDistance);
+                position = Camera.main.ScreenToWorldPoint(position);
+                //Random.Range();
+
+                //Creating our bullet object in the world
+                GameObject go1 = Instantiate(bulletPrefab, bulletMuzzle.transform.position, Quaternion.identity) as GameObject;
+                GameObject go2 = Instantiate(bulletPrefab, bulletMuzzle.transform.position, Quaternion.identity) as GameObject;
+                GameObject go3 = Instantiate(bulletPrefab, bulletMuzzle.transform.position, Quaternion.identity) as GameObject;
+                GameObject go4 = Instantiate(bulletPrefab, bulletMuzzle.transform.position, Quaternion.identity) as GameObject;
+                GameObject go5 = Instantiate(bulletPrefab, bulletMuzzle.transform.position, Quaternion.identity) as GameObject;
+                GameObject go6 = Instantiate(bulletPrefab, bulletMuzzle.transform.position, Quaternion.identity) as GameObject;
+
+
+               
+
+                //setting the bullet objects direction to be twowards the mouse position
+                go1.transform.LookAt(position + new Vector3(Random.Range(-2,2), Random.Range(-2,2)));
+                go2.transform.LookAt(position + new Vector3(Random.Range(-2, 2), Random.Range(-2, 2)));
+                go3.transform.LookAt(position + new Vector3(Random.Range(-2, 2), Random.Range(-2, 2)));
+                go4.transform.LookAt(position + new Vector3(Random.Range(-2, 2), Random.Range(-2, 2)));
+                go5.transform.LookAt(position + new Vector3(Random.Range(-2, 2), Random.Range(-2, 2)));
+                go6.transform.LookAt(position + new Vector3(Random.Range(-2, 2), Random.Range(-2, 2)));
+
+                //Remove debug when not needed
+                Debug.Log(position);
+                //Add forward force to the bullet
+                go1.GetComponent<Rigidbody2D>().AddForce(go1.transform.forward * 1500);
+                go2.GetComponent<Rigidbody2D>().AddForce(go2.transform.forward * 1500);
+                go3.GetComponent<Rigidbody2D>().AddForce(go3.transform.forward * 1500);
+                go4.GetComponent<Rigidbody2D>().AddForce(go4.transform.forward * 1500);
+                go5.GetComponent<Rigidbody2D>().AddForce(go5.transform.forward * 1500);
+                go6.GetComponent<Rigidbody2D>().AddForce(go6.transform.forward * 1500);
+
+                //After a certain amount of seconds, destroy bullet
+                Destroy(go1, 1.0f);
+                Destroy(go2, 1.0f);
+                Destroy(go3, 1.0f);
+                Destroy(go4, 1.0f);
+                Destroy(go5, 1.0f);
+                Destroy(go6, 1.0f);
+  
+            }
+
+
+           
+        }
 
 
 
