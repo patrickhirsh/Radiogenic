71a2899481f4b9112c2e35786749ffc38dfff4b8
Tyler <mr.hoskino@gmail.com>
2018-02-27 13:05:15 -0700

    Changed shotgun
    added more bullets and changed the button to 0 so that it is left click

--- SpaceBlastersPrototype/Assets/Scripts/PLayerSynthControls.cs
+++ SpaceBlastersPrototype/Assets/Scripts/PLayerSynthControls.cs
@@ -54,8 +54,8 @@ public class PLayerSynthControls : MonoBehaviour
 
         velocity.x = 0;
         velocity.y = 0;
-        //		velocity.x = Input.GetAxis("Horizontal") * velocityRate;
-        //		velocity.y = Input.GetAxis("Vertical") * velocityRate;
+        //        velocity.x = Input.GetAxis("Horizontal") * velocityRate;
+        //        velocity.y = Input.GetAxis("Vertical") * velocityRate;
         if (Input.GetKey(KeyCode.W))
         {
             velocity.y = 10f * velocityRate;
@@ -122,10 +122,12 @@ public class PLayerSynthControls : MonoBehaviour
         }
         GetComponent<Rigidbody2D>().AddForce(velocity);
 
-        if (Time.time > interval + 1f)
-            shotgun = true;
+        if (Time.time > interval + 0.5f)
+        {
+         shotgun = true;
+        }
 
-        if (Input.GetMouseButtonDown(1) && shotgun)
+        if (Input.GetMouseButtonDown(0) && shotgun)
         {
 
 
@@ -149,6 +151,12 @@ public class PLayerSynthControls : MonoBehaviour
             GameObject go4 = Instantiate(bulletPrefab, bulletMuzzle.transform.position, Quaternion.identity) as GameObject;
             GameObject go5 = Instantiate(bulletPrefab, bulletMuzzle.transform.position, Quaternion.identity) as GameObject;
             GameObject go6 = Instantiate(bulletPrefab, bulletMuzzle.transform.position, Quaternion.identity) as GameObject;
+            GameObject go7 = Instantiate(bulletPrefab, bulletMuzzle.transform.position, Quaternion.identity) as GameObject;
+            GameObject go8 = Instantiate(bulletPrefab, bulletMuzzle.transform.position, Quaternion.identity) as GameObject;
+            GameObject go9 = Instantiate(bulletPrefab, bulletMuzzle.transform.position, Quaternion.identity) as GameObject;
+            GameObject go10 = Instantiate(bulletPrefab, bulletMuzzle.transform.position, Quaternion.identity) as GameObject;
+            GameObject go11 = Instantiate(bulletPrefab, bulletMuzzle.transform.position, Quaternion.identity) as GameObject;
+            GameObject go12 = Instantiate(bulletPrefab, bulletMuzzle.transform.position, Quaternion.identity) as GameObject;
 
 
 
@@ -164,6 +172,12 @@ public class PLayerSynthControls : MonoBehaviour
             go4.transform.LookAt(position);
             go5.transform.LookAt(position);
             go6.transform.LookAt(position);
+            go7.transform.LookAt(position);
+            go8.transform.LookAt(position);
+            go9.transform.LookAt(position);
+            go10.transform.LookAt(position);
+            go11.transform.LookAt(position);
+            go12.transform.LookAt(position);
 
             //go1.transform.Rotate(new Vector3(0, 0, Random.Range(-2, 2)));
             //go2.transform.Rotate(new Vector3(0, 0, Random.Range(-2, 2)));
@@ -185,6 +199,12 @@ public class PLayerSynthControls : MonoBehaviour
             float vari4 = Random.Range(-variance, variance);
             float vari5 = Random.Range(-variance, variance);
             float vari6 = Random.Range(-variance, variance);
+            float vari7 = Random.Range(-variance, variance);
+            float vari8 = Random.Range(-variance, variance);
+            float vari9 = Random.Range(-variance, variance);
+            float vari10 = Random.Range(-variance, variance);
+            float vari11 = Random.Range(-variance, variance);
+            float vari12 = Random.Range(-variance, variance);
 
             //Add forward force to the bullet
             go1.GetComponent<Rigidbody2D>().AddForce(new Vector2(thrust * Mathf.Cos(angleR + vari1), thrust * Mathf.Sin(angleR + vari1)));
@@ -193,6 +213,12 @@ public class PLayerSynthControls : MonoBehaviour
             go4.GetComponent<Rigidbody2D>().AddForce(new Vector2(thrust * Mathf.Cos(angleR + vari4), thrust * Mathf.Sin(angleR + vari4)));
             go5.GetComponent<Rigidbody2D>().AddForce(new Vector2(thrust * Mathf.Cos(angleR + vari5), thrust * Mathf.Sin(angleR + vari5)));
             go6.GetComponent<Rigidbody2D>().AddForce(new Vector2(thrust * Mathf.Cos(angleR + vari6), thrust * Mathf.Sin(angleR + vari6)));
+            go7.GetComponent<Rigidbody2D>().AddForce(new Vector2(thrust * Mathf.Cos(angleR + vari7), thrust * Mathf.Sin(angleR + vari7)));
+            go8.GetComponent<Rigidbody2D>().AddForce(new Vector2(thrust * Mathf.Cos(angleR + vari8), thrust * Mathf.Sin(angleR + vari8)));
+            go9.GetComponent<Rigidbody2D>().AddForce(new Vector2(thrust * Mathf.Cos(angleR + vari9), thrust * Mathf.Sin(angleR + vari9)));
+            go10.GetComponent<Rigidbody2D>().AddForce(new Vector2(thrust * Mathf.Cos(angleR + vari10), thrust * Mathf.Sin(angleR + vari10)));
+            go11.GetComponent<Rigidbody2D>().AddForce(new Vector2(thrust * Mathf.Cos(angleR + vari11), thrust * Mathf.Sin(angleR + vari11)));
+            go12.GetComponent<Rigidbody2D>().AddForce(new Vector2(thrust * Mathf.Cos(angleR + vari12), thrust * Mathf.Sin(angleR + vari12)));
             //After a certain amount of seconds, destroy bullet
             Destroy(go1, 1.0f);
             Destroy(go2, 1.0f);
@@ -200,6 +226,12 @@ public class PLayerSynthControls : MonoBehaviour
             Destroy(go4, 1.0f);
             Destroy(go5, 1.0f);
             Destroy(go6, 1.0f);
+            Destroy(go7, 1.0f);
+            Destroy(go8, 1.0f);
+            Destroy(go9, 1.0f);
+            Destroy(go10, 1.0f);
+            Destroy(go11, 1.0f);
+            Destroy(go12, 1.0f);
 
 
 
