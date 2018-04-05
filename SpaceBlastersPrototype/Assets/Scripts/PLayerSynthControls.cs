f09fecc00efbf2e7d2ec06019018dc885edae090
Tyler <mr.hoskino@gmail.com>
2018-04-05 13:44:43 -0600

    BlackHole added
    needs to change the velocity

--- SpaceBlastersPrototype/Assets/Scripts/PLayerSynthControls.cs
+++ SpaceBlastersPrototype/Assets/Scripts/PLayerSynthControls.cs
@@ -8,6 +8,7 @@ public class PLayerSynthControls : MonoBehaviour
     public GameObject bulletMuzzle;
     public GameObject reflectiveBullet;
     public GameObject bulletPrefab;
+    public GameObject blackHole;
     private Vector2 velocity;
     public float velocityRate = 20f;
     public float accelerationRate;
@@ -18,11 +19,12 @@ public class PLayerSynthControls : MonoBehaviour
     float dashInterval = 0f;
     bool shotgun = false;
     bool dash = true;
-    bool blackHolePowerUp = false;
+    bool blackHolePowerUp = true;
     bool reflectiveBulletPowerUp = false;
     bool powerUpInUse = false;
     public int thrust = 1000;
     public int thrust2 = 1200;
+    public int blackHoleThrust = 300;
     public float variance = 0.2f;
     public float variance2 = 1f;
     public int hp = 10;
@@ -374,11 +376,26 @@ public class PLayerSynthControls : MonoBehaviour
             shotGunInterval = Time.time;
 
         }
-        else if (reflectiveBulletPowerUp && powerUpInUse && Input.GetMouseButtonDown(0))         {
+        else if (reflectiveBulletPowerUp && powerUpInUse == false && Input.GetKey(KeyCode.B))         {
             // instatiate reflective bullet objects.
-            powerUpTimer = Time.time;             powerUpInUse = false;          }         else if (reflectiveBulletPowerUp && Input.GetMouseButton(0))         {             if (Time.time > powerUpTimer + 10f)             {                 reflectiveBulletPowerUp = false;             }             Vector3 position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0.0f);             position = Camera.main.ScreenToWorldPoint(position);             float vari1 = Random.Range(-variance, variance);             float angleR = Mathf.Atan((position.y - transform.position.y) / (position.x - transform.position.x));             if ((position.x - transform.position.x) < 0)                 angleR += Mathf.PI;              GameObject go1 = Instantiate(reflectiveBullet, bulletMuzzle.transform.position, bulletMuzzle.transform.rotation) as GameObject;             go1.transform.LookAt(position);             go1.GetComponent<Rigidbody2D>().AddForce(new Vector2(thrust2 * Mathf.Cos(angleR + vari1), thrust2 * Mathf.Sin(angleR + vari1)));             Destroy(go1, 3.0f);         }
-        else if(blackHolePowerUp && powerUpInUse){
-            
+            powerUpTimer = Time.time;             powerUpInUse = true;          }         else if (reflectiveBulletPowerUp && powerUpInUse && Input.GetMouseButton(0))         {             if (Time.time > powerUpTimer + 10f)             {                 reflectiveBulletPowerUp = false;
+                powerUpInUse = false;             }             Vector3 position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0.0f);             position = Camera.main.ScreenToWorldPoint(position);             float vari1 = Random.Range(-variance, variance);             float angleR = Mathf.Atan((position.y - transform.position.y) / (position.x - transform.position.x));             if ((position.x - transform.position.x) < 0)                 angleR += Mathf.PI;              GameObject go1 = Instantiate(reflectiveBullet, bulletMuzzle.transform.position, bulletMuzzle.transform.rotation) as GameObject;             go1.transform.LookAt(position);             go1.GetComponent<Rigidbody2D>().AddForce(new Vector2(thrust2 * Mathf.Cos(angleR + vari1), thrust2 * Mathf.Sin(angleR + vari1)));             Destroy(go1, 3.0f);         }
+        else if(blackHolePowerUp&& Input.GetKey(KeyCode.B)){
+
+            Vector3 position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0.0f);
+            position = Camera.main.ScreenToWorldPoint(position);
+            float vari1 = Random.Range(-variance, variance);
+            float angleR = Mathf.Atan((position.y - transform.position.y) / (position.x - transform.position.x));
+            if ((position.x - transform.position.x) < 0)
+                angleR += Mathf.PI;
+                
+            GameObject go1 = Instantiate(blackHole, bulletMuzzle.transform.position, bulletMuzzle.transform.rotation) as GameObject;
+            go1.transform.LookAt(position);
+
+            go1.GetComponent<Rigidbody2D>().AddForce(new Vector2(blackHoleThrust * Mathf.Cos(angleR + vari1), blackHoleThrust * Mathf.Sin(angleR + vari1)));
+
+            blackHolePowerUp = false;
+            Destroy(go1, 15.0f);
         } 
         else if (Input.GetMouseButton(0))
         {
---
+++ SpaceBlastersPrototype/Assets/Scripts/blackHoleScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blackHoleScript : MonoBehaviour {
+
+    public float radius = 5.0f;
+    public float power = 10.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+		
+	}
+    void explode()
+    {
+        
+        //do the exploding
+        Vector3 explosionPos = transform.position;
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(explosionPos, radius);
+        foreach (Collider2D hit in colliders)
+        {
+
+            Rigidbody2D rb = hit.GetComponent<Rigidbody2D>();
+
+            var direction = Vector3.zero;
+            if (rb != null)
+            {
+                direction = rb.transform.position - transform.position;
+
+                rb.AddForce(-direction.normalized * power, ForceMode2D.Impulse);
+             
+            }
+        }
+
+    }
+}
