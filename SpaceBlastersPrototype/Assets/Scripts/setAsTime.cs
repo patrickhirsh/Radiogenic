326569c3e2bab56ee7489262c2934c5dd9b1a8db
Patrick Hirsh <hirshpatrick@gmail.com>
2018-04-26 12:56:59 -0600

    Merged changes for UI elements and GameState reset implementation

--- SpaceBlastersPrototype/Assets/Scripts/setAsTime.cs
+++ SpaceBlastersPrototype/Assets/Scripts/setAsTime.cs
@@ -22,7 +22,7 @@ public class setAsTime : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () 
     {
-        text.text = "Time " + (int)Time.time;
+        text.text = "Time " + (int)GameManager.getGameTime();
 
 	}
 }
