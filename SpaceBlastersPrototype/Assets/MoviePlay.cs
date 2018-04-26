f3f6e6c6e1869346f9dc908dd009208e61498440
greyvest <greyvestanderson@gmail.com>
2018-04-26 12:45:33 -0600

    almost there

---
+++ SpaceBlastersPrototype/Assets/MoviePlay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoviePlay : MonoBehaviour {
+
+	// Use this for initialization
+
+    public MovieTexture movTexture;
+    void Start()
+    {
+        GetComponent<Renderer>().material.mainTexture = movTexture;
+        movTexture.Play();
+    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
