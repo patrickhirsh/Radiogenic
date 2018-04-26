e026a6cd8f0b6420b05d399dfbb28e875c849236
greyvest <greyvestanderson@gmail.com>
2018-04-26 13:37:14 -0600

    SICK INTRO

---
+++ SpaceBlastersPrototype/Assets/NewBehaviourScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    int timtout = 11;
+    double counter = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        counter += .02;
+        Debug.Log(counter);
+        if(counter >= timtout){
+            SceneManager.LoadScene("Start Mewn");
+        }
+	}
+}
